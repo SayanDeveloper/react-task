4c429db18f3d5038fd2619a295decdb0f39f7af9
Sayan Samanta <samantasayan23@gmail.com>
2022-03-02 15:08:39 +0530

    react template setup

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# react-task
+## ReactJs Task
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      ReactJs task - mezchip
+    </div>
+  );
+}
+
+export default App;
